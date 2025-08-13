1eb1927bd57433dfcfcef623f243823dce0b1e50
Andrea26-9 <la.zavalamartinez@ugto.mx>
2025-08-13 16:03:41 -0600

    Add files via upload

---
+++ Semana2/Ago13_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){ 
+ int a = 3; 
+ int b = 4; 
+ int c = 5; 
+ int d = 7; 
+  int e; 
+
+
+ printf("e = %d\n", (a+b)*c/d); 
+ printf("e = %d\n", ((a+b)*c/d)); 
+ printf("e = %d\n", a+b*c/d); 
+ printf ("e = %d\n", a+(b*c)/d); 
+ }
+  
+
+  
