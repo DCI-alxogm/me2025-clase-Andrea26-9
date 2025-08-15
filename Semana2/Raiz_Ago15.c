e6eca400c2bdcdb05d7ac05121ddffb34fe14d65
Andrea26-9 <la.zavalamartinez@ugto.mx>
2025-08-15 11:53:39 -0600

    Add files via upload

---
+++ Semana2/Raiz_Ago15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+  float a, b, c, x1, x2, discr; 
+  printf("Ingrese el valor de A:"); 
+  scanf("%f", &a); 
+  scanf("%f", &b); 
+  scanf("%f", &c); 
+
+
+  if(a==0){
+    if(b==0){
+      printf("Solucion trivial"); 
+    } else 
+
+
+   }
+    }
