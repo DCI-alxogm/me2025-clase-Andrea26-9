090a49832ee343e90150ccc42f1775cbaf5fe057
Andrea26-9 <la.zavalamartinez@ugto.mx>
2025-08-19 11:59:30 -0600

    Add files via upload

---
+++ Semana 3/Ejercicio_Ago19.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () { 
+  int x, Es; 
+  printf("Ingrese el valor de x: "); 
+  scanf("%d", &x); 
+  printf("Ingrese el valor de Es: "); 
+  scanf("%d", Es; 
+  
+  }
